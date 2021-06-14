1c4bb60b042882b3a8668599a978ef2ec9dbda89
unknown <you@example.com>
2021-06-14 18:40:45 +0600

    first commit

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvBTxdNZR55WoSBOGWPFQYoX_O6fJBcLM",
+    authDomain: "fixman-js.firebaseapp.com",
+    projectId: "fixman-js",
+    storageBucket: "fixman-js.appspot.com",
+    messagingSenderId: "988452602562",
+    appId: "1:988452602562:web:ab84301cb4b3928e4b715b"
+};
+// Initialize Firebase
+
+export default firebaseConfig;
